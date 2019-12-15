946ceaf985d4d939ce021062ae7c91120bd61329
UdayakumarU <erudayu@gmail.com>
2019-12-15 11:33:20 +0530

    folder organized

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CartCard from './app'
-import 'bootstrap/dist/css/bootstrap.css';
-import './css/theme.css';
-
-
-ReactDOM.render(<CartCard/>, document.getElementById('root'));
