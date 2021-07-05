79d973ae755d7d2843b71cb5e6fc1e55355d4d8d
UdayakumarU <erudayu@gmail.com>
2021-07-05 23:57:56 +0530

    added cart button

--- ReactApp/src/components/misc/header.jsx
+++ ReactApp/src/components/misc/header.jsx
@@ -6,9 +6,17 @@ const Header = () => (
         <Link to={'/'}>
             <img src={"../logo.png"} alt="UKART" style={{ width: "8rem" }} className="img-responsive" />
         </Link>
-        <Link to={'/user/login'}>
-            <button className="btn btn-outline-dark">Login</button>
-        </Link>
+        <div className ="my-2">
+            <Link to={'/user/login'} className="mr-sm-2">
+                <button className="btn btn-outline-dark">Login</button>
+            </Link>
+            <Link to={'/cart'} className ="my-2 my-sm-0">
+                <button className="btn btn-dark">
+                    <i className="material-icons">shopping_cart</i>
+                    <span className="align-straight">Cart</span>
+                </button>
+            </Link>
+        </div>
     </nav>
  )
 
