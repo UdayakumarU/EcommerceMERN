914d1ba1885f05bcec1a973ff3dd5f4756e0fc35
UdayakumarU <erudayu@gmail.com>
2021-07-16 00:29:30 +0530

    showing different empty cart section based on login status

--- ReactApp/src/components/cart/emptyCart.jsx
+++ ReactApp/src/components/cart/emptyCart.jsx
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom' ;
+import { connect } from 'react-redux';
 
 import { Tile } from "../../library";
+import { getCustomerLoginStatus } from "../../redux/customer/customer.selector";
+
+const mapStateToProps = (state) => ({
+    Loggedin: getCustomerLoginStatus(state)
+});
 
 class EmptyCart extends Component {
-    render() {
-
+    
+    loggedInCartSection = () =>{
         return (
-            <Tile className ="container mt-3"> 
+            <React.Fragment>
+                <div className="text-center">
+                    <p className="blockquote mb-0 mt-3">Your cart is empty!</p>
+                    <p><small>Add items to it now</small></p>
+                </div>
+                <div className="offset-md-2 col-md-8">
+                    <Link to="/" className="btn btn-block _primary_bg"> Shop now </Link>
+                </div>
+            </React.Fragment>
+        )
+    }
+    
+    loggedOutCartSection = () =>{
+        return (
+            <React.Fragment>
+                <div className="text-center">
+                    <p className="blockquote mb-0 mt-3">Missing Cart items?</p>
+                    <p><small>Login to see the items you added previously</small></p>
+                </div>
+                <div className="offset-md-2 col-md-8">
+                    <Link to="/user/login" className="btn btn-block _primary_bg"> Login </Link>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+    render() {
+        const { Loggedin } = this.props;
+        return (
+            <Tile className ="container mt-4"> 
                 <div className ="row justify-content-md-center">
-                    <div className ="col-md-3 mt-3">
+                    <div className ="col-md-4 mt-4 mb-4">
                         <div className = "text-center">
                             <img src={"../empty-cart.png"} alt="Empty cart" style={{ width: "13rem" }} className="img-responsive" />
-                            <p className = "blockquote mb-0 mt-3">Your cart is empty!</p>
-                            <p><small>Add items to it now</small></p>
+                            { Loggedin? this.loggedInCartSection(): this.loggedOutCartSection() }
                         </div>
-                        <Link to = "/" className="btn btn-block _primary_bg"> 
-                            Shop now 
-                        </Link>
                     </div>
                 </div>
             </Tile>
@@ -25,4 +56,4 @@ class EmptyCart extends Component {
     }
 }
 
-export default EmptyCart
+export default connect(mapStateToProps)(EmptyCart);
