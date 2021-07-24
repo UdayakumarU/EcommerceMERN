54cb63bdf7b19e5a4bab0e88bc232c4e9344a6ff
UdayakumarU <erudayu@gmail.com>
2021-07-24 22:22:02 +0530

    render cart items

---
+++ ReactApp/src/components/cart/cartItemList.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { Tile } from "../../library";
+import { getCartItems } from "../../redux/cart/cart.selector";
+
+import {calculatePriceAfterDiscount} from "../../utils/util";
+
+const mapStateToProps = (state) => ({
+    cartItems: getCartItems(state)
+});
+
+class CartItemList extends Component {
+    render() {
+        const { cartItems } = this.props;
+        return (
+            <Tile className ="container mt-4"> 
+                <h5>{"My Cart"}</h5>
+                {cartItems.map(item => {
+                    const {brand, productName, productActualPrice, discount, productId, productImages} = item;
+                    return(<div key={productId}>
+                            <hr/>
+                            <div className="mt-2 row">
+                                <div className ="col-md-2">
+                                    <img className="img-thumbnail" src={productImages[0]} alt={productName} />
+                                </div>
+                                <div className ="col-md-6">
+                                    <p className="blockquote _cut_text">{productName}</p>
+                                    <h6 className="text-muted">{brand}</h6>
+                                    <div> 
+                                        <strong className="mr-2">₹{calculatePriceAfterDiscount(productActualPrice, discount)}</strong>
+                                        <s className="mr-2">{productActualPrice}</s>
+                                        <small className="text-success"><strong>{discount}% off</strong></small>  
+                                    </div>
+                                </div>
+                                <div className ="col-md-3">
+                                    <p><small>Delivery in 2 days, Mon | ₹40 </small></p>
+                                </div>
+                                <div className ="col-md-1">
+                                    <div className="_absolute_top_right">
+                                        <span className="btn text-danger _danger_hover">
+                                            <span className="material-icons">delete</span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>)
+                    })
+                }
+            </Tile>
+        )
+    }
+}
+
+export default connect(mapStateToProps)(CartItemList);
--- ReactApp/src/pages/cartPage.jsx
+++ ReactApp/src/pages/cartPage.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 import Footer from "../components/misc/footer";
 import Header from "../components/misc/header";
 import EmptyCart from "../components/cart/emptyCart";
+import CartItemList from "../components/cart/cartItemList";
+
+import { getCartItems } from '../redux/cart/cart.selector';
+
+const mapStateToProps = (state) => ({
+    cartItems : getCartItems(state)
+});
 
 class CartPage extends Component {
     render(){
+        const { cartItems } = this.props;
         return (
             <React.Fragment>
                 <Header hideCart={true}/>
-                <EmptyCart/>
+                { (cartItems.length > 0)? <div className="col-md-8"> <CartItemList/> </div>: <EmptyCart/> }
                 <Footer/>
             </React.Fragment>
         );
     }
 }
 
-export default CartPage;
+export default connect(mapStateToProps)(CartPage);
