36e08bc8a81611709975f1c8eec9aab26c5abf2c
UdayakumarU <erudayu@gmail.com>
2021-08-16 23:57:09 +0530

    added edit button for address list

--- ReactApp/src/components/checkout/deliveryAddress/addressList.jsx
+++ ReactApp/src/components/checkout/deliveryAddress/addressList.jsx
@@ -11,17 +11,24 @@ export default class AddressList extends Component {
         const {addresses} = this.props;
         return (
             addresses.map(address => (
-                <Tile key={address._id}>
-                    <div className="small">
-                        <span className="mx-2"><strong>{ address.receiverName }</strong></span>
-                        {this.getAddressType(address.addressType)}
-                        <span className="mx-2"><strong>{ address.receiverMobile }</strong></span>
-                    </div>
-                    <div className="small my-3">
-                        <span>{ `${address.area}, ` }</span>
-                        <span>{ `${address.city}, ` }</span>
-                        <span>{ `${address.state} - ` }</span>
-                        <span><strong>{ address.pincode }</strong></span>
+                <Tile key={address._id} >
+                    <div className="row">
+                        <div className="col-md-10">
+                            <div className="small">
+                                <span className="mx-2"><strong>{ address.receiverName }</strong></span>
+                                {this.getAddressType(address.addressType)}
+                                <span className="mx-2"><strong>{ address.receiverMobile }</strong></span>
+                            </div>
+                            <div className="small my-3">
+                                <span>{ `${address.area}, ` }</span>
+                                <span>{ `${address.city}, ` }</span>
+                                <span>{ `${address.state} - ` }</span>
+                                <span><strong>{ address.pincode }</strong></span>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <button className="btn btn-light text-primary float-right _text_sm_dark">EDIT</button>
+                        </div>
                     </div>
                 </Tile>
             ))
